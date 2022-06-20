fa7e3b0dd3234edeb0cc38b6e8cec58b6f934b20
MuhammadHARIS46 <muhammadhariskuk123@gmail.com>
2022-06-20 07:50:07 -0700

    password hashing and JWT authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+const JWT_SECRET="harisisagoodboy$";
 // Crteate a User using :POST "/api/auth" Does not require authemtication
 const router = express.Router();
 router.post(
@@ -23,13 +26,21 @@ router.post(
       if(user){
         return res.status(400).json({error:"sorry user with email exists already"})
       }
+      const salt= await bcrypt.genSalt(10);
+      const secPass=await bcrypt.hash(req.body.password ,salt);
       user= await User.create({
         name: req.body.name,
         email:req.body.email,
-        password: req.body.password,
+        password: secPass
       })
-      
-      res.json(user)
+      const data={
+        user:{
+          id:user.id
+        }
+      }
+      var authToken=jwt.sign({authToken},JWT_SECRET);
+      res.json({authToken})
+      // res.json(user)
     } catch (error) {
       console.error(error.message)
       res.status(500).send("some error occured")
