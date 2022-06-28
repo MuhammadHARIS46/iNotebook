e17f982895814c043767efb1e1e698d9b781e1cf
MuhammadHARIS46 <muhammadhariskuk123@gmail.com>
2022-06-28 03:29:13 -0700

    get user data after login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,6 +4,7 @@ const { body, validationResult } = require("express-validator");
 const bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 const JWT_SECRET="harisisagoodboy$";
+const fetchUser=require("../middleware/fetchUser");
 // Crteate a User using :POST "/api/auth" Does not require authemtication
 const router = express.Router();
 router.post(
@@ -47,4 +48,57 @@ router.post(
     }
   }
 );
+// Authenticate a User using :POST "/api/auth/login" Does not require login
+router.post(
+  '/login',
+  [
+    // body("name","enter a valid name").isLength({ min: 3 }),
+    body("email","enter a valid email").isEmail(),
+    body("password","password can not be blank").exists(),
+  ], async (req, res) => {
+    // if there are errors return bad request and the error
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {email,password} = req.body;
+    try {
+      let user= await User.findOne({email});
+      if (!user){
+        return res.status(400).json( {error:"please try to login with correct ceredentials"});
+      }
+      const PasswrodCompare=  await bcrypt.compare(password,user.password);
+      if (!PasswrodCompare){
+        return res.status(400).json( {error:"please try to login with correct ceredentials"});
+      }
+      const data={
+        user:{
+          id:user.id
+        }
+      }
+      var authToken=jwt.sign({authToken},JWT_SECRET);
+      res.json({authToken})
+    } catch (error) {
+      console.error(error.message)
+      res.status(500).send("some error occured")
+    }
+
+
+    
+// get user details using :POST "/api/auth/getuser" require login
+    router.post(
+      '/getuser', fetchUser,
+      async(req,res)=>{
+        try {
+          userId=req.user.id;
+          const user=await User.findById(userId).select("-password"); //take everything except password
+          res.send(user)
+        } catch (error) {
+          console.error(error.message)
+          res.status(500).send("some error occured")
+        }
+
+      }
+    )
+  })
 module.exports = router;
