fea57c166f3e55be1296f10a67bc477e4215043f
MuhammadHARIS46 <muhammadhariskuk123@gmail.com>
2022-08-26 12:15:45 -0700

    delete note functionality implemented and modal created to edit note

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 const connectToMongo=require("./db");
 const express = require('express')
+var cors = require('cors')
 connectToMongo();
 const app = express()
 const port = 5000
+
+
+
+
+app.use(cors())
+
 
 app.use(express.json()) //necessary when we use req.body in router files
 app.use('/api/auth',require('./routes/auth'))
